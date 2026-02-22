e98dd16f80325c375bd0464d2e5ed7bbaf77a652
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-02-22 11:20:13 -0600

    se establecieron los rangos

--- Ejercicio 2/Program.cs
+++ Ejercicio 2/Program.cs
@@ -14,12 +14,35 @@ Console.WriteLine("Autorización escrita (s/n)");
 string autori = Console.ReadLine();
 Console.WriteLine("Trae USB (s/n)");
 string usb = Console.ReadLine();
-if (usb == "s")
+if (usb == "s" || usb == "S")
 {
     Console.WriteLine("Está acompañado (s/n)");
+    string acompañado = Console.ReadLine();
 }
-else
+int acceso = int.Parse(Console.ReadLine());
+
+if (rol < 1 || rol > 4)
 {
-    Console.WriteLine("Ingrese");
+    Console.WriteLine("Tipo de rol no válido");
+}
+else if (hora < 0 || hora > 23)
+{
+    Console.WriteLine("Horas fuera de rango");
+}
+else if (dia < 1 || dia > 7)
+{
+    Console.WriteLine("Dias no validos");
+}
+else if (carnet != "s" && carnet != "S" && carnet != "n" && carnet != "N")
+{
+    Console.WriteLine("Carnet debe ser s o n");
+}
+else if (autori != "s" && autori != "S" && autori != "n" && autori != "N ")
+{
+    Console.WriteLine("La autorización debe ser s o n");
+}
+else if (usb != "s" && usb != "S" && usb != "n" && usb != "N")
+{
+    Console.WriteLine("USB tiene que ser s o n");
 }
 
